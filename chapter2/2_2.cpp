a2cfbd054888a3d07cb0ac22230c34f6ce90bef5
seanleecn <45760771+seanleecn@users.noreply.github.com>
2019-12-17 22:39:17 +0800

    add 2-1 and 2-2

---
+++ chapter2/2_2.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void display(vector<int> vec)
+{
+    for (int ix = 0; ix <= vec.size(); ix++){
+        cout << vec[ix] << ' ';
+    }
+    cout << endl;
+}
+
+void swap(int val1, int val2)
+{
+    int temp;
+    temp = val1;
+    val1 = val2;
+    val2 = temp;
+}
+
+void bubble_sort()
+{
+    for(){
+        for (){
+            
+        }
+    }
+}
+
+int main ()
+{
+    int ia[8] = {8,34,3,13,1,21,5,2};
+    //用arry作为vector的初值
+    vector<int> vec
+    return 0;
+}
